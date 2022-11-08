038f0f2965c007faa56903c7f80fc5553a49d7b4
alanzarate <alnzarate@gmail.com>
2022-11-08 00:55:10 -0400

    funcionando hasta drag and drop

--- src/main/java/com/example/backend/controller/ProductController.java
+++ src/main/java/com/example/backend/controller/ProductController.java
@@ -1,20 +1,47 @@
 package com.example.backend.controller;
 
+import com.example.backend.entity.ImageModel;
 import com.example.backend.entity.Product;
 import com.example.backend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @PostMapping({"/addNewProduct"})  // addNewProduct  /product/new
-    public Product addNewProduct(@RequestBody Product product){
-        return productService.addNewProduct(product);
+    @PostMapping(value = {"/addNewProduct"}, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})  // addNewProduct  /product/new
+    public Product addNewProduct(@RequestPart("product") Product product,
+                                 @RequestPart("imageFile")MultipartFile[] file){
+
+        try{
+            Set<ImageModel> images = uploadImage(file);
+            product.setProductImages(images);
+            return productService.addNewProduct(product);
+        }catch (Exception e){
+            System.out.println("(Controller/ProductController) ->addNewProduct " + e.getMessage());
+            return null;
+        }
+    }
+
+    public Set<ImageModel> uploadImage(MultipartFile[] multipartFiles) throws IOException {
+        Set<ImageModel> imageModels = new HashSet<>();
+        for(MultipartFile file: multipartFiles){
+            ImageModel imageModel = new ImageModel(
+                    file.getOriginalFilename(),
+                    file.getContentType(),
+                    file.getBytes()
+            );
+            imageModels.add(imageModel);
+        }
+
+        return imageModels;
     }
 }
